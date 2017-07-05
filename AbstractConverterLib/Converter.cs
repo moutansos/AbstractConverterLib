1575e70cfacb486c3ac69c17423ae21b8ca7f404
Ben Brougher <moutansos@gmail.com>
2017-07-04 22:32:00 -0700

    start work on better converter arcitecture

---
+++ AbstractConverterLib/Converter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractConverterLib
+{
+    public class Converter
+    {
+        private Func<string, string> StringToString;
+        private Func<bool, string> BoolToString;
+        private Func<byte, string> ByteToString;
+        private Func<int, string> IntToString;
+        private Func<float, string> FloatToString;
+        private Func<double, string> DoubleToString;
+        private Func<decimal, string> DecimalToString;
+
+        public Converter()
+        {
+
+        }
+
+        #region To String Properties
+        public Func<string, string> StringToStringMethod
+        {
+            set => StringToString = value;
+        }
+
+        public Func<bool, string> BoolToStringMethod
+        {
+            set => BoolToString = value;
+        }
+        #endregion
+    }
+}
