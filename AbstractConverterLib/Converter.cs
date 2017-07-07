f6b749ba03cdd7338f56ce086ef7e849901aaa23
ben <moutansos@gmail.com>
2017-07-06 21:03:27 -0700

    finished work on router class and implemented it. Adding converter tests

--- AbstractConverterLib/Converter.cs
+++ AbstractConverterLib/Converter.cs
@@ -8,164 +8,81 @@ namespace AbstractConverterLib
 {
     public class AbstractConverter
     {
-        #region To String Methods
-        private Func<string, string> StringToString;
-        private Func<bool, string> BoolToString;
-        private Func<byte, string> ByteToString;
-        private Func<int, string> IntToString;
-        private Func<float, string> FloatToString;
-        private Func<double, string> DoubleToString;
-        private Func<decimal, string> DecimalToString;
-        private Func<sbyte, string> SbyteToString;
-        private Func<long, string> LongToString;
-        private Func<short, string> ShortToString;
-        private Func<uint, string> UintToString;
-        private Func<ulong, string> UlongToString;
-        private Func<ushort, string> UshortToString;
-        private Func<char, string> CharToString;
-        #endregion
-
         TypedMethodRouter router;
 
         public AbstractConverter()
         {
-            StringToString = ConvToString.StringToString;
-            BoolToString = ConvToString.BoolToString;
-            ByteToString = ConvToString.ByteToString;
-            IntToString = ConvToString.IntToString;
-            FloatToString = ConvToString.FloatToString;
-            DoubleToString = ConvToString.DoubleToString;
-            DecimalToString = ConvToString.DecimalToString;
-            SbyteToString = ConvToString.SbyteToString;
-            LongToString = ConvToString.LongToString;
-            ShortToString = ConvToString.ShortToString;
-            UintToString = ConvToString.UintToString;
-            UlongToString = ConvToString.UlongToString;
-            UshortToString = ConvToString.UshortToString;
-            CharToString = ConvToString.CharToString;
-
             router = new TypedMethodRouter();
+
+            #region Default To String Methods
             router.AddMethod<string, string>(ConvToString.StringToString);
+            router.AddMethod<bool, string>(ConvToString.BoolToString);
+            router.AddMethod<byte, string>(ConvToString.ByteToString);
+            router.AddMethod<int, string>(ConvToString.IntToString);
+            router.AddMethod<float, string>(ConvToString.FloatToString);
+            router.AddMethod<double, string>(ConvToString.DoubleToString);
+            router.AddMethod<decimal, string>(ConvToString.DecimalToString);
+            router.AddMethod<sbyte, string>(ConvToString.SbyteToString);
+            router.AddMethod<long, string>(ConvToString.LongToString);
+            router.AddMethod<short, string>(ConvToString.ShortToString);
+            router.AddMethod<uint, string>(ConvToString.UintToString);
+            router.AddMethod<ulong, string>(ConvToString.UlongToString);
+            router.AddMethod<ushort, string>(ConvToString.UshortToString);
+            router.AddMethod<char, string>(ConvToString.CharToString);
+            #endregion
         }
-
-        #region To String Properties
-        public Func<string, string> StringToStringMethod
-        {
-            set => StringToString = value;
-        }
-
-        public Func<bool, string> BoolToStringMethod
-        {
-            set => BoolToString = value;
-        }
-
-        public Func<byte, string> ByteToStringMethod
-        {
-            set => ByteToString = value;
-        }
-
-        public Func<int, string> IntToStringMethod
-        {
-            set => IntToString = value;
-        }
-
-        public Func<float, string> FloatToStringMethod
-        {
-            set => FloatToString = value;
-        }
-
-        public Func<double, string> DoubleToStringMethod
-        {
-            set => DoubleToString = value;
-        }
-
-        public Func<decimal, string> DecimalToStringMethod
-        {
-            set => DecimalToString = value;
-        }
-
-        public Func<sbyte, string> SbtyeToStringMethod
-        {
-            set => SbyteToString = value;
-        }
-
-        public Func<long, string> LongToStringMethod
-        {
-            set => LongToString = value;
-        }
-
-        public Func<short, string> ShortToStringMethod
-        {
-            set => ShortToString = value;
-        }
-
-        public Func<uint, string> UintToStringMethod
-        {
-            set => UintToString = value;
-        }
-
-        public Func<ulong, string> UlongToStringMethod
-        {
-            set => UlongToString = value;
-        }
-
-        public Func<ushort, string> UshortToStringMethod
-        {
-            set => UshortToString = value;
-        }
-
-        public Func<char, string> CharToStringMethod
-        {
-            set => CharToString = value;
-        }
-        #endregion
 
         public OUT Conv<IN, OUT>(IN dataIn)
         {
-            if(typeof(OUT) == typeof(string))
+            if(router.ContainsRoute(typeof(IN), typeof(OUT)))
             {
-                return ToStringRouter<OUT, IN>(dataIn);
+                return (OUT)Convert.ChangeType(router.Call(dataIn, typeof(IN), typeof(OUT)), typeof(OUT));
             }
             else
             {
-                throw new UnableToConvertTypeException("Unable to convert the type " + typeof(IN) + " to the type of " + typeof(OUT));
+                throw new UnableToConvertTypeException("Unable to convert " + typeof(IN) + " to " + typeof(OUT));
             }
         }
 
-        private OUT ToStringRouter<OUT, IN>(IN dataIn)
+        public OUT Conv<OUT>(object dataIn)
         {
-            if(typeof(IN) == typeof(string))
+            if (router.ContainsRoute(dataIn.GetType(), typeof(OUT)))
             {
-
+                return (OUT)Convert.ChangeType(router.Call(dataIn, dataIn.GetType(), typeof(OUT)), typeof(OUT));
             }
             else
             {
-                throw new UnableToConvertTypeException("Unable to convert the type " + typeof(IN) + " to the type of " + typeof(OUT));
+                throw new UnableToConvertTypeException("Unable to convert " + dataIn.GetType() + " to " + typeof(OUT));
             }
         }
     }
 
     public class TypedMethodRouter
     {
-        private Dictionary<Tuple<Type, Type>, Func<object, object>> dict;
+        private Dictionary<Tuple<Type, Type>, dynamic> dict;
 
         public TypedMethodRouter()
         {
-            dict = new Dictionary<Tuple<Type, Type>, Func<object, object>>();
+            dict = new Dictionary<Tuple<Type, Type>, dynamic>();
         }
 
         public void AddMethod<IN, OUT>(Func<IN, OUT> method)
         {
             Tuple<Type, Type> key = new Tuple<Type, Type>(typeof(IN), typeof(OUT));
-            dict[key] = (Func<object, object>)Convert.ChangeType(method, typeof(Func<object, object>));
+            dict[key] = method;
         }
 
         public Func<IN, OUT> GetMethod<IN, OUT>()
         {
             Tuple<Type, Type> key = new Tuple<Type, Type>(typeof(IN), typeof(OUT));
-            Func<object, object> unconverted = dict[key];
-            Func<IN, OUT> converted = (Func<IN, OUT>)Convert.ChangeType(unconverted, typeof(Func<IN, OUT>));
+            Func<IN, OUT> converted = dict[key];
             return converted;
+        }
+
+        public object Call(dynamic inVal, Type inType, Type outType)
+        {
+            Tuple<Type, Type> key = new Tuple<Type, Type>(inType, outType);
+            return dict[key](inVal);
         }
 
         public bool ContainsRoute(Type inType, Type outType)
--- UnitTest/ToStringTests.cs
+++ UnitTest/ToStringTests.cs
@@ -8,6 +8,7 @@ namespace UnitTest
     [TestClass]
     public class ToStringTests
     {
+        #region String To String Tests
         [TestMethod]
         public void StringToStringDataField()
         {
@@ -18,6 +19,26 @@ namespace UnitTest
         }
 
         [TestMethod]
+        public void StringToStringConverterObj()
+        {
+            string original = "This is a test string!";
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<string, string>(original);
+            Assert.AreEqual(original, converted);
+        }
+
+        [TestMethod]
+        public void StringToStringConverterObjSingleParam()
+        {
+            string original = "This is a test string!";
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<string>(original);
+            Assert.AreEqual(original, converted);
+        }
+        #endregion
+
+        #region Bool To String Tests
+        [TestMethod]
         public void BoolToStringDataField()
         {
             bool original = true;
@@ -26,6 +47,26 @@ namespace UnitTest
             Assert.AreEqual(original.ToString(), converted);
         }
 
+        [TestMethod]
+        public void BoolToStringConverterObj()
+        {
+            bool original = true;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<bool, string>(original);
+            Assert.AreEqual(original.ToString(), converted);
+        }
+
+        [TestMethod]
+        public void BoolToStringConverterObjSingleParam()
+        {
+            bool original = true;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<string>(original);
+            Assert.AreEqual(original.ToString(), converted);
+        }
+        #endregion
+
+        #region Byte To String Tests
         [TestMethod]
         public void ByteToStringDataField()
         {
@@ -36,6 +77,26 @@ namespace UnitTest
         }
 
         [TestMethod]
+        public void ByteToStringConverterObj()
+        {
+            byte original = 34;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<byte, string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+
+        [TestMethod]
+        public void ByteToStringConverterObjSingleParam()
+        {
+            byte original = 34;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+        #endregion
+
+        #region Int To String Tests
+        [TestMethod]
         public void IntToStringDataField()
         {
             int original = -123124891;
@@ -44,6 +105,26 @@ namespace UnitTest
             Assert.AreEqual(original + "", converted);
         }
 
+        [TestMethod]
+        public void IntToStringConverterObj()
+        {
+            int original = -324342334;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<int, string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+
+        [TestMethod]
+        public void IntToStringConverterSingleParam()
+        {
+            int original = -324342334;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+        #endregion
+
+        #region Float To String Tests
         [TestMethod]
         public void FloatToStringDataField()
         {
@@ -54,6 +135,26 @@ namespace UnitTest
         }
 
         [TestMethod]
+        public void FloatToStringConverterObj()
+        {
+            float original = -1249999999999999999;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<float, string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+
+        [TestMethod]
+        public void FloatToStringConverterObjSingleParam()
+        {
+            float original = -1249999999999999999;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+        #endregion
+
+        #region Double To String Tests
+        [TestMethod]
         public void DoubleToStringDataField()
         {
             double original = -2901391283904032333;
@@ -62,6 +163,26 @@ namespace UnitTest
             Assert.AreEqual(original + "", converted);
         }
 
+        [TestMethod]
+        public void DoubleToStringConverterObj()
+        {
+            double original = -1249999999999999999;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<double, string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+
+        [TestMethod]
+        public void DoubleToStringConverterObjSingleParam()
+        {
+            double original = -1249999999999999999;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+        #endregion
+
+        #region Decimal To String Tests
         [TestMethod]
         public void DecimalToStringDataField()
         {
@@ -72,6 +193,26 @@ namespace UnitTest
         }
 
         [TestMethod]
+        public void DecimalToStringConverterObj()
+        {
+            decimal original = -1249999999999999999;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<decimal, string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+
+        [TestMethod]
+        public void DecimalToStringConverterObjSingleParam()
+        {
+            decimal original = -1249999999999999999;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+        #endregion
+
+        #region Sbyte To String Tests
+        [TestMethod]
         public void SbyteToStringDataField()
         {
             sbyte original = -25;
@@ -79,6 +220,25 @@ namespace UnitTest
             string converted = field.ToType<string>();
             Assert.AreEqual(original + "", converted);
         }
+
+        [TestMethod]
+        public void SbyteToStringConverterObj()
+        {
+            sbyte original = -12;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<sbyte, string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+
+        [TestMethod]
+        public void SbyteToStringConverterObjSingleParam()
+        {
+            sbyte original = -12;
+            AbstractConverter conv = new AbstractConverter();
+            string converted = conv.Conv<sbyte, string>(original);
+            Assert.AreEqual(original + "", converted);
+        }
+        #endregion
 
         [TestMethod]
         public void LongToStringDataField()
